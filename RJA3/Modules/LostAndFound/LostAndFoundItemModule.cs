bbaa31ad30a8c99850a418418fc77060d2a0fb7c
jasser <houimlijasser9@gmail.com>
2026-01-28 18:03:50 +0100

    Update

--- RJA3/Modules/LostAndFound/LostAndFoundItemModule.cs
+++ RJA3/Modules/LostAndFound/LostAndFoundItemModule.cs
@@ -2,9 +2,9 @@
 using RJA3.Modules.LostAndFound.Features.LostItems.Persistence;
 using RJA3.Modules.LostAndFound.Features.LostItems.ReportLostItem;
 
-namespace RJA3.Modules
+namespace RJA3.Modules.LostAndFound
 {
-    public static class LostItemModule
+    public static class LostAndFoundItemModule
     {
 
         public static IServiceCollection AddLostItemServices(this IServiceCollection services)
